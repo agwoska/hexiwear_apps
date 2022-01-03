53c8bb6937023076ec56d16dfa3d3e205e275b5d
Woska <60791044+agwoska@users.noreply.github.com>
2022-01-03 00:23:30 -0500

    Create new HTU driver

---
+++ Hexi_HTU21D/HTU21D.cpp
@@ -0,0 +1,68 @@
+/**
+ * @file HTU21D.cpp
+ * @author Andrew Woska (agwoska@buffalo.edu)
+ * @date Jan 02, 2022
+ * @brief implementation of the HTU21D for
+ *      Hexiwear development in Mbed OS 5.12
+ * 
+ * documentation: https://cdn-shop.adafruit.com/datasheets/1899_HTU21D.pdf
+ * 
+ * last updated: 
+ */
+
+#include "HTU21D.h"
+
+HTU21D::HTU21D(PinName sda, PinName scl):
+    i2c(sda, scl) {
+        i2c.frequency(HTU21D_FREQ);
+    }
+
+HTU21D::HTU21D() :
+    i2c(PTB1, PTB0) {
+        i2c.frequency(HTU21D_FREQ);
+    };
+
+HTU21D::~HTU21D() {
+    delete &i2c;
+}
+
+int HTU21D::getTemp() {
+    char tx[1];
+    char rx[2];
+
+    // send temperature trigger
+    tx[0] = HTU21D_TRIG_TEMP;
+    i2c.write((HTU21D_I2C_ADDR << 1) & HTU21D_SOFT_RESET, tx, 1);
+    ThisThread::sleep_for(50ms); // (max time req pg. 5)
+
+    // read measurement
+    i2c.read((HTU21D_I2C_ADDR << 1) | 1, rx, 2);
+    ThisThread::sleep_for(1ms);
+
+    // compute temp (pg. 15)
+    unsigned int rawTemp = ((unsigned int)tx[0] << 8) | (unsigned int)rx[1];
+    rawTemp &= 0xFFFC; // remove status bits
+    float actTemp = -46.85 + 175.72 * rawTemp / (float)(1 << 16);
+    return (int)actTemp;
+
+}
+
+int HTU21D::getHumidity() {
+    char tx[1];
+    char rx[2];
+
+    // send temperature trigger
+    tx[0] = HTU21D_TRIG_HUMD;
+    i2c.write((HTU21D_I2C_ADDR << 1) & HTU21D_SOFT_RESET, tx, 1);
+    ThisThread::sleep_for(16ms); // (max time req pg. 3)
+
+    // read measurement
+    i2c.read((HTU21D_I2C_ADDR << 1) | 1, rx, 2);
+    ThisThread::sleep_for(1ms);
+
+    // compute temp (pg. 15)
+    unsigned int rawHumd = ((unsigned int)tx[0] << 8) | (unsigned int)rx[1];
+    rawHumd &= 0xFFFC; // remove status bits
+    return -6 + 125 * rawHumd / (1 << 16);
+
+}
---
+++ Hexi_HTU21D/HTU21D.h
@@ -0,0 +1,88 @@
+/**
+ * @file HTU21D.h
+ * @author Andrew Woska (agwoska@buffalo.edu)
+ * @date Jan 02, 2022
+ * @brief header file of the HTU21D for
+ *      Hexiwear development in Mbed OS 5.12
+ * 
+ * datasheet: https://cdn-shop.adafruit.com/datasheets/1899_HTU21D.pdf
+ * 
+ * last updated: 
+ */
+
+#pragma once
+
+#ifndef HTU21D_H
+#define HTU21D_H
+
+#include "mbed.h"
+
+
+/** constants **/
+
+/* from datasheet */
+
+// 0.4 MHz (pg. 9)
+#define HTU21D_FREQ         0x61A80
+
+// command table (pg. 11)
+#define HTU21D_TRIG_TEMP    0xE3
+#define HTU21D_TRIG_HUMD    0xE5
+#define HTU21D_TRIG_TEMP_NH 0xF3
+#define HTU21D_TRIG_HUMD_NH 0xF5
+#define HTU21D_WRITE_REG    0xE6
+#define HTU21D_READ_REG     0xE7
+#define HTU21D_SOFT_RESET   0xFE
+
+// sending commands (pg. 10)
+#define HTU21D_I2C_ADDR     0x40
+#define HTU21D_WRITE_ADDR   0x80
+#define HTU21D_READ_ADDR    0x81
+
+
+/* class prototype */
+
+class HTU21D {
+
+public:
+
+    /**
+     * Construct a new HTU21D object
+     * @param sda pin for SDA of I2C
+     * @param scl pin for SCL of I2C
+     */
+    HTU21D(PinName sda, PinName scl);
+
+    /**
+     * Construct a new HTU21D object
+     * with default Hexiwear pins
+     */
+    HTU21D();
+
+    /**
+     * Destroy the HTU21D object
+     */
+    ~HTU21D();
+
+    /**
+     * Get the relative humidity as a percent
+     * @return relative humidity 
+     */
+    int getHumidity();
+
+    /**
+     * Get measured temperature in Celcius
+     * @return temperature
+     */
+    int getTemp();
+
+private:
+
+    /**
+     * I2C object used for measurements
+     */
+    I2C i2c;
+
+};
+
+#endif // HTU21D_H
